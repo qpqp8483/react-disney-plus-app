ca0b8cd72137de81a4ef127cea5e8645a0303af7
chanwoo <wkdcksdn1996@gmail.com>
2023-01-31 01:28:49 +0900

    배너 생성

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,7 @@ import axios from "../api/axios";
 import React, { useEffect } from "react";
 import requests from "../api/request";
 import { useState } from "react";
+import "./Banner.css";
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
@@ -28,7 +29,33 @@ const Banner = () => {
     console.log(movie);
   };
 
-  return <div>Banner</div>;
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + "..." : str;
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("http://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+        <div className="banner__buttons">
+          {movie?.videos?.results[0]?.key && (
+            <button className="banner__buttons paly">Play</button>
+          )}
+        </div>
+        <p className="banner__description">{truncate(movie.overview, 100)}</p>
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </header>
+  );
 };
 
 export default Banner;
