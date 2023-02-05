6d2acea505c1a897a5abd08c56c948a11993fbda
chanwoo <qpqp8483@naver.com>
2023-02-05 17:44:42 +0900

    router 작업, login 작업중

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 
 const NavWrapper = styled.nav`
@@ -29,22 +30,48 @@ const Logo = styled.a`
   }
 `;
 
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  transition: all 0.2s ease 0s;
+  &:hover {
+    background-color: #f9f9f9;
+    color: gray;
+    border-color: transparent;
+  }
+`;
+const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: rgba(0, 0, 0, 0.582);
+  border-radius: 5px;
+  color: #fff;
+  padding: 5px;
+  border: none;
+`;
+
 const Nav = () => {
   const [show, setShow] = useState(false);
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 50) {
-        setShow(true);
-      } else {
-        setShow(false);
-      }
-    });
-
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener("scroll", () => {});
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
 
   return (
     <NavWrapper show={show}>
@@ -55,6 +82,12 @@ const Nav = () => {
           onClick={() => (window.location.href = "/")}
         />
       </Logo>
+
+      {pathname === "/" ? (
+        <Login>Login</Login>
+      ) : (
+        <Input className="nav__input" type="text" placeholder="검색해주세요." />
+      )}
     </NavWrapper>
   );
 };
