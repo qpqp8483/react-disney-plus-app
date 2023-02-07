c3371256048748a00e0fe70b874526708ec82fe0
chanwoo <wkdcksdn1996@gmail.com>
2023-02-07 23:21:24 +0900

    slide 잣업

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,7 +1,33 @@
-import React from "react";
+import axios from "../../api/axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const DetailPage = () => {
-  return <div>DetailPage</div>;
+  let { movieId } = useParams();
+  const [movie, setMovie] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`/movie/${movieId}`);
+      setMovie(response.data);
+      console.log(response);
+    }
+    fetchData();
+  }, [movieId]);
+
+  if (!movie) {
+    return null;
+  }
+
+  return (
+    <section>
+      <img
+        className="modal__poster-img"
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt="modal__poster-img"
+      />
+    </section>
+  );
 };
 
 export default DetailPage;
